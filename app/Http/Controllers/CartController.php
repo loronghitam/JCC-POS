70dbe5566109bd6be09e26b9c19b47544cadc017
hariyadiwijaya1 <hariyadiwijaya1@gmail.com>
2022-10-21 14:35:43 +0700

    asu

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        return view('cart');
+    }
 
+    public function show()
+    {
+        return view('cart.show');
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([]);
+    }
+
+    public function destroy(Request $request)
+    {
+        $request->validate([
+            'cart_id' =>'required'
+        ]);
+    }
 }
